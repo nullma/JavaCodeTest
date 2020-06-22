196c0c06ae5819c9fcf3c86ddb57ef420462974d
mawentao <11915805@qq.com>
2020-06-22 12:30:42 +0800

    Initial commit

--- basic_code/src/nx/jy/demo01/DemoStringEquils.java
+++ basic_code/src/nx/jy/demo01/DemoStringEquils.java
@@ -1,5 +1,11 @@
 package nx.jy.demo01;
 
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+import org.w3c.dom.ls.LSOutput;
+
 public class DemoStringEquils {
     //创建测试文件 git
+    public static void main(String[] args) {
+        System.out.println("git git !!!");
+    }
 }
