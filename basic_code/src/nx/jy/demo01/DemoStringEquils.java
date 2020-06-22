3c259b2405996a07adb245be89cc5ae0800fa247
mawentao <11915805@qq.com>
2020-06-22 12:56:11 +0800

    String 常用方法

--- basic_code/src/nx/jy/demo01/DemoStringEquils.java
+++ basic_code/src/nx/jy/demo01/DemoStringEquils.java
@@ -4,9 +4,9 @@ import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
 import org.w3c.dom.ls.LSOutput;
 
 public class DemoStringEquils {
-    //创建测试文件 git
     public static void main(String[] args) {
         System.out.println("git git !!!");
         System.out.println("git git !!!");
     }
+    //创建测试文件 git
 }
--- basic_code/src/nx/jy/demo01/DemoStringGet.java
+++ basic_code/src/nx/jy/demo01/DemoStringGet.java
@@ -7,5 +7,27 @@ public class DemoStringGet {
     public static void main(String[] args) {
         int length = "sdfsdfdsfhjweihfd".length();
         System.out.println("字符串的内容"+length);
+        System.out.println("====================");
+
+        //字符串拼接 返回新的字符串
+        String str1 = "mawen";
+        String str2 = "tao";
+
+        String str3 = str1.concat(str2);
+        System.out.println(str3);
+        System.out.println("====================");
+
+        // 获取指定位置的的字符
+        char ch = "hello".charAt(1);
+        System.out.println(ch);
+        System.out.println("====================");
+
+        // 查找字符串中 字符第一次出现的位置的索引
+        String original = "helloWorld";
+        int wor = original.indexOf("llo");
+        System.out.println(wor);
+        System.out.println("====================END");
+
+
     }
 }
